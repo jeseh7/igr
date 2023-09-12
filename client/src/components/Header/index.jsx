be9bf0845af3498cb8db9b342cad907aa34f4a87
cfinn7789 <cfinn7789@gmail.com>
2023-09-12 13:39:15 -0400

    fixed header link error

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 //import logoImage from './controller.png';//
 import LoginModal from './login';
 import SignUpModal from './sign-up';
