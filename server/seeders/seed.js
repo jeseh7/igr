5d553f5b416144a9ad3cfd7f81f3617d39cbbea6
JoFel7 <josuefeliciano7@gmail.com>
2023-09-14 20:33:59 -0400

    Resolver work and seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -3,7 +3,7 @@ const { Game, Review, User } = require("../models");
 const gameSeeds = require("./gameSeeds.json");
 const userSeeds = require('./userSeeds.json');
 const reviewSeeds = require("./reviewSeeds.json");
-const gameDB = require("./gameDB");
+const gameDB = require("./cleanDB");
 
 db.once("open", async () => {
   try {
@@ -13,17 +13,17 @@ db.once("open", async () => {
     await User.create(userSeeds);
     await Game.create(gameSeeds);
 
-    // for (let i = 0; i < reviewSeeds.length; i++) {
-    //   const { _id, reviewAuthor } = await Review.create(reviewSeeds[i]);
-    //   const user = await User.findOneAndUpdate(
-    //     { username: reviewAuthor },
-    //     {
-    //       $addToSet: {
-    //         reviews: _id,
-    //       },
-    //     }
-    //   );
-    // }
+    for (let i = 0; i < reviewSeeds.length; i++) {
+      const { _id, reviewAuthor } = await Review.create(reviewSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: reviewAuthor },
+        {
+          $addToSet: {
+            reviews: _id,
+          },
+        }
+      );
+    }
   } catch (err) {
     console.error(err);
     process.exit(1);
