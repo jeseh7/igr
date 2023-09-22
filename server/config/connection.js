e976b3b9662da8c10fec1bcf18df3e6c26e397c0
JoFel7 <josuefeliciano7@gmail.com>
2023-09-22 19:29:13 -0400

    fixed authemtication

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/igr');
+mongoose.connect(process.env.PORT || 'mongodb://127.0.0.1:27017/igr');
 
 module.exports = mongoose.connection;
